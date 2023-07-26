49735ea5789d22f9a272aaf5b11d7249b230ee88
Ade Oktaviano Arrahman <adeoktaviano50@gmail.com>
2023-07-26 10:18:46 +0700

    adding dashboard guest 3

--- guest/dashboard.php
+++ guest/dashboard.php
@@ -200,10 +200,11 @@
               </div>
             </div>
           </div>
-
+        </div>
+                </div>
         <!-- cards row 2 -->
-        <div class="flex flex-wrap mt-6 -mx-3">
-          <div class="w-full max-w-full px-3 mt-0 lg:w-7/12 lg:flex-none">
+        <div class="flex w-full justify-center  flex-wrap mt-6 ">
+          <div class="w-full max-w-full px-3 mt-0 lg:flex-none" style="width : 90%;">
             <div class="border-black/12.5 dark:bg-slate-850 dark:shadow-dark-xl shadow-xl relative z-20 flex min-w-0 flex-col break-words rounded-2xl border-0 border-solid bg-white bg-clip-border">
               <div class="border-black/12.5 mb-0 rounded-t-2xl border-b-0 border-solid p-6 pt-4 pb-0">
                 <h6 class="capitalize dark:text-white">Sales overview</h6>
